a918dde4b0f0a8958293579e2a64ea96f2d7d037
yuval almaliah <yuv.almaliah@hmail.com>
2019-01-26 10:28:49 +0200

    add legacy struckture

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 const port = process.env.PORT || 3535;
 app.use('/', express.static('angular-app/dist'));
-app.use('/legacy', express.static('angularjs-app/src'));
+app.use('/legacy', express.static('angularjs-app'));
 
 app.listen(port, function () {
   console.log(' this  app listening on port ' + port)
